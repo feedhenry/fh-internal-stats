40c4ac6faec50a17df65a38b57891f36bce1c26a
Gerard Ryan <gryan@redhat.com>
2015-06-22 17:08:28 +0100

    Build using Grunt instead of Make

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  // Just set shell commands for running different types of tests
+  grunt.initConfig({});
+
+  grunt.loadNpmTasks('grunt-fh-build');
+  grunt.registerTask('default', ['fh:default']);
+};
